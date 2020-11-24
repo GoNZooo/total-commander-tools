51c4dd65a5d0154939d5308e7d66795a9b3ce19e
Rickard Andersson <gonz@severnatazvezda.com>
2020-11-24 22:49:49 +0200

    use parameter tools in `show-parameters` executable

--- src/show-parameters.zig
+++ src/show-parameters.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
+const debug = std.debug;
+const heap = std.heap;
+
+const parameters = @import("./parameters.zig");
 
 pub fn main() anyerror!void {
-    const parameters = try std.process.argsAlloc(std.heap.page_allocator);
-    std.debug.print("Arguments:\n", .{});
-    for (parameters) |p, i| {
-        std.debug.print("{}: {}\n", .{ i, p });
+    const arguments = try std.process.argsAlloc(std.heap.page_allocator);
+    const sourcesAndTarget = try parameters.getSourcesAndTarget(heap.page_allocator, arguments[1..]);
+
+    debug.print("Sources:\n", .{});
+    for (sourcesAndTarget.sources) |source, i| {
+        debug.print("\t{}: {}\n", .{ i, source.value });
     }
+    debug.print("Target: {}\n", .{sourcesAndTarget.target.value});
 
     _ = try std.io.getStdIn().reader().readByte();
 }
